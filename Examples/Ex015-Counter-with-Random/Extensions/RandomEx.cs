8205923a27cb1ff2db2e620e94ecc6c2b8a940ab
Hue Holleran <hholleran@glasswallsolutions.com>
2018-10-08 20:25:45 +0100

    Add counter examples

---
+++ Examples/Ex015-Counter-with-Random/Extensions/RandomEx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace Ex015_Counter_with_Random.Extensions
+{
+    static class RandomEx
+    {
+        private static int seed = Environment.TickCount;
+
+        private static ThreadLocal<Random> randomWrapper = new ThreadLocal<Random>(() =>
+            new Random(Interlocked.Increment(ref seed))
+        );
+
+        /// <summary>
+        /// Gets a random instance for thread.
+        /// </summary>
+        /// <returns>Random from <c>ThreadLocal</c></returns>
+        public static Random GetThreadRandom()
+        {
+            return randomWrapper.Value;
+        }
+    }
+}
---
+++ Examples/Ex015-Counter-with-Random/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Ex015_Counter_with_Random.Extensions;
+using Huesie.ConsoleSPA;
+
+namespace Ex015_Counter_with_Random
+{
+    class Program
+    {
+        static long _counter = 100;
+
+        static bool _randomSteps = false;
+        static int _step = 1;
+
+        static void Main(string[] args)
+        {
+            // Setup event loop
+            EventLoop.SetTitle = () => $"Counter example with Random ({(_randomSteps ? $"*ON*: Random step of *{_step}*" : "off")})";
+            EventLoop.SetKeysHelp = () => "KEYS: *R*andom toggle - *S*et step";
+
+            // OnRefresh - runs every ~1sec
+            EventLoop.OnRefresh = () => {
+                ConsoleEx.WriteBlankingLine();
+
+                _counter += (!_randomSteps) ? 0 : (RandomEx.GetThreadRandom().Next(2) * 2 - 1) * _step; // +/- step
+                ConsoleEx.WriteLineEnh($"Counter: *{_counter}*");
+
+                ConsoleEx.WriteBlankingLine();
+            };
+
+            // Key presses
+            EventLoop.OnKey = (key) => {
+                switch (key.ToUpperInvariant()) {
+                    case "R": RandomStepsToggle(); break;
+                    case "S": SetStepValue(); break;
+                }
+            };
+
+            EventLoop.Run();
+        }
+
+        static void RandomStepsToggle()
+        {
+            ConsoleEx.WriteLineEnh("Toggling random steps mode..");
+            EventLoop.ShortDelay();
+            _randomSteps = !_randomSteps;
+            Console.Clear();
+        }
+
+        static void SetStepValue()
+        {
+            ConsoleEx.WriteEnh("(non-int or blank will cancel) *Set value of each random step*: ");
+            var stringValue = Console.ReadLine();
+            int.TryParse(!string.IsNullOrWhiteSpace(stringValue) ? stringValue : _counter.ToString(), out _step);
+            Console.Clear();
+        }
+    }
+}
